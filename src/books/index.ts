ae7788feedb82ed92339eaa4e67fe7185ab30669
Alex Van Kralingen <alexvankralingen@gmail.com>
2024-07-02 17:53:30 +0000

    forgot to save file

--- src/books/index.ts
+++ src/books/index.ts
@@ -3,17 +3,18 @@ import booksList from './list'
 import createOrUpdateBook from './create_or_update'
 import deleteBook from './delete'
 import getBookRoute from './lookup'
+import { type BookDatabaseAccessor } from '../database_access'
 
-export function setupBookRoutes (router: ZodRouter): void {
+export function setupBookRoutes (router: ZodRouter, books: BookDatabaseAccessor): void {
   // Setup Book List Route
-  booksList(router)
+  booksList(router, books)
 
   // Setup Book Create Route
-  createOrUpdateBook(router)
+  createOrUpdateBook(router, books)
 
   // Setup Book Delete Route
-  deleteBook(router)
+  deleteBook(router, books)
 
   // Lookup Book
-  getBookRoute(router)
+  getBookRoute(router, books)
 }
