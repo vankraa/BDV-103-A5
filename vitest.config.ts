f111d4822efc0abba68310fdcfc4b0922d06ce5d
Alex Van Kralingen <alexvankralingen@gmail.com>
2024-07-02 13:29:12 +0000

    added vitest-openapi-plugin and workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import generateOpenApi from './vitest-openapi-plugin'
 
 export default defineConfig({
+  plugins: [
+    generateOpenApi
+  ],
+  server: {
+    watch: {
+      ignored: ['client/**']
+    }
+  },
   test: {
     includeSource: ['src/**/*.{js,ts}'],
     setupFiles: ['./database_test_setup.ts']
